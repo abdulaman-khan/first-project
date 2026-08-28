26b3d75de7561995385bd9cfea422a11b8c466a1
abdulaman-khan <abbasaman385@gmail.com>
2026-08-28 19:50:57 +0530

    Code Update

---
+++ JS/String_Palindrom.js
@@ -0,0 +1,12 @@
+let str = "madam";
+let reverse = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+}
+
+if (str === reverse) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
