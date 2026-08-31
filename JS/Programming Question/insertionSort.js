6dec2e62e0df49609f4543e562a9ee1f9cbe8475
abdulaman-khan <abbasaman385@gmail.com>
2026-08-31 21:33:40 +0530

    InsertionSort

---
+++ JS/Programming Question/insertionSort.js
@@ -0,0 +1,15 @@
+let arr = [12, 11, 13, 5, 6];
+
+for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > current) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+
+    arr[j + 1] = current;
+}
+
+console.log("Sorted Array:", arr);
