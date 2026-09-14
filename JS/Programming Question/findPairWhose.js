31cd98d8d85ea03229923627db819c77e85b19b1
abdulaman-khan <abbasaman385@gmail.com>
2026-09-14 18:50:33 +0530

    Find Pair Whose Sum is Equal to Target

---
+++ JS/Programming Question/findPairWhose.js
@@ -0,0 +1,12 @@
+// Find Pair Whose Sum is Equal to Target
+
+let arr = [2, 7, 11, 15, 3, 6];
+let target = 9;
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === target) {
+            console.log(arr[i], arr[j]);
+        }
+    }
+}
